5684835cdb8255c1c27f1af21317944e6b73e802
Suraj D M <surajdm@Surajs-MacBook-Air.local>
2021-09-01 19:35:20 -0400

    Fixed test cases

--- test/test.py
+++ test/test.py
@@ -1,3 +1,8 @@
+import sys, os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
 from code.ArrayComputations import ArrayComputations
 
 def test_increasing_sort():
