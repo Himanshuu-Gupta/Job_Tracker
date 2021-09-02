dab7ec8279e64ca12dad5d18d9adea2cdd512606
Himanshu Gupta <himanshu199586@gmail.com>
2021-09-02 01:18:35 -0400

    Created setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+		name="SE_Fraction_Force",
+		version="0.1",
+		description="Module contain basic array functions(sorting, reverse sorting, count freq",
+		author="Himanshu Gupta",
+		author_email="himanshu199586@gmail.com",
+		url="https://github.com/Himanshuu-Gupta/SE_Fraction_Force",
+		license="MIT",
+		packages=["SE_Fraction_Force"],
+		long_description="""\
+				Module contain basic array manipulation functions:
+						* Sorting
+						* Reverse Sorting
+						* Count frequency
+		""",
+		classifiers=[
+      "License :: OSI Approved :: MIT License",
+			"Programming Language :: Python",
+			"Development Status :: 1 - Planning",
+			"Intended Audience :: Developers",
+			"Topic :: Array"
+		],
+		keywords="sorting reverse sorting count frequency",
+		install_requires=[]
+	)
